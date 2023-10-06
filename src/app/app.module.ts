d66ca178f25672d30bb87b17e0632e7a5bec9b50
Oto Tchokhonelidze <oto.tchokhonelidze@programmingo.com>
2023-10-06 19:57:15 +0400

    get started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannersComponent } from './components/banners/banners.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannersComponent
   ],
   imports: [
     BrowserModule,
