97d2bfd898c4c7c964bd91e94cd0f81b16624262
oto48 <otchokhonelidze4@gmail.com>
2023-10-16 17:21:38 +0400

    add new token

--- src/app/services/banner.service.ts
+++ src/app/services/banner.service.ts
@@ -12,7 +12,7 @@ export class BannerService {
   private blobDownloadUrl = 'https://development.api.optio.ai/api/v2/blob'; 
 
   private authToken =
-    'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoib3RjaG9raG9uZWxpZHplNEBnbWFpbC5jb20iLCJpYXQiOjE2OTY1Njg5MjcsImV4cCI6MTY5NzQzMjkyN30.y1JIrlwozUMnOKFcLjmDmvuA1NFml2XQXQpQahzY5GdH6XuOywEuaCW9UZZzG0HVyIVoBhbESXcDoNj3aJXPIw';
+    'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoib3RjaG9raG9uZWxpZHplNEBnbWFpbC5jb20iLCJpYXQiOjE2OTc0MzAyODAsImV4cCI6MTY5ODI5NDI4MH0.g26ka0D00Y53c9tEKJPshf5gn7uXn1OAKCSkw-R7rXUF3YqTPWNWRVu-0NKyr2aEh4_T_SfcfmfkQJKnCXwfjw';
 
   constructor(private http: HttpClient) {}
 
